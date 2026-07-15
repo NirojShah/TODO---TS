b98cb82b2386c85a2d7d44a21c7951f43ac44b38
Niraj Shah <nirajshah950@gmail.com>
2026-07-15 15:20:25 +0530

    fix: update todo route

--- server/src/module/todo/todo.route.ts
+++ server/src/module/todo/todo.route.ts
@@ -1,14 +1,31 @@
 import { Router } from "express";
 import type { Router as RouterType } from "express";
 import TodoController from "./todo.controller.js";
+import type { Request, Response } from "express";
 
 const todoRoute: RouterType = Router();
 
-const todoController: TodoController = new TodoController();
+const todoController = new TodoController();
 
-todoRoute.post("/", todoController.createTodo);
-todoRoute.get("/", todoController.getAllTodo);
-todoRoute.patch("/", todoController.updateTodo);
-todoRoute.delete("/", todoController.deleteTodo);
+todoRoute.post(
+  "/",
+  async (req: Request, res: Response, next) =>
+    await todoController.createTodo(req, res, next),
+);
+todoRoute.get(
+  "/",
+  async (req: Request, res: Response, next) =>
+    await todoController.getAllTodo(req, res, next),
+);
+todoRoute.patch(
+  "/",
+  async (req: Request, res: Response, next) =>
+    await todoController.updateTodo(req, res, next),
+);
+todoRoute.delete(
+  "/",
+  async (req: Request, res: Response, next) =>
+    await todoController.deleteTodo(req, res, next),
+);
 
 export default todoRoute;
